692c242225e52e9f851d321b4dff5400c49ba241
siph <dawkins.chris.dev@gmail.com>
2022-08-09 01:13:58 -0600

    better markdown grammar

--- lib/buildPlugin.nix
+++ lib/buildPlugin.nix
@@ -11,7 +11,7 @@
     p.tree-sitter-nix
     p.tree-sitter-python
     p.tree-sitter-rust
-    p.tree-sitter-markdown
+    p.tree-sitter-markdown-inline
     p.tree-sitter-comment
     p.tree-sitter-toml
     p.tree-sitter-make
