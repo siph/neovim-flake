f80d4350a60597dca839c05e1551269208657132
siph <dawkins.chris.dev@gmail.com>
2023-02-08 08:23:09 -0700

    rollback to non-broken nightly build

--- flake.nix
+++ flake.nix
@@ -5,7 +5,8 @@
 
     neovim-overlay = {
       url = "github:nix-community/neovim-nightly-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.url = "github:nixos/nixpkgs?rev=fad51abd42ca17a60fc1d4cb9382e2d79ae31836";
+      #inputs.nixpkgs.follows = "nixpkgs";
     };
 
     # LSP plugins
