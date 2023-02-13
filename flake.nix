cad7f1a2cdd45d813e1dda5115b7cf27b6118e76
siph <dawkins.chris.dev@gmail.com>
2023-02-13 03:11:23 -0700

    unpin neovim-nightly overlay

--- flake.nix
+++ flake.nix
@@ -1,12 +1,11 @@
 {
   description = "Chris' Neovim Configuration";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/master";
 
     neovim-overlay = {
-      url = "github:nix-community/neovim-nightly-overlay";
-      inputs.nixpkgs.url = "github:nixos/nixpkgs?rev=fad51abd42ca17a60fc1d4cb9382e2d79ae31836";
-      #inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nix-community/neovim-nightly-overlay/master";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     # LSP plugins
